a04c931b6bf95506f3fdd642c3cff4113a56c01a
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-08 11:03:55 +0000

    feature: Create base server with express Install express as a dependency Install default npm packages associated with project after cloning

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // implement your API here
+const express = require('express');
+const server = express();
+
+const userDB = require('./data/db');
+const port = 5000;
+
+server.use(express.json());
+
