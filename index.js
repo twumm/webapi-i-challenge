5a8a4075bc5267811f2fc6a3a9586916c1859006
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-08 11:22:28 +0000

    feature: Add all routes with a response sent for sanity checking

--- index.js
+++ index.js
@@ -7,3 +7,30 @@ const port = 5000;
 
 server.use(express.json());
 
+server.post('/api/users', (req, res) => {
+  res.send('post new user');
+})
+
+server.get('/api/users', (req, res) => {
+  res.send('get all users');
+})
+
+server.get('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  res.send(`get user with ${id}`);
+})
+
+server.delete('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  res.send(`delete user with id ${id}`);
+})
+
+server.put('/api/users/:id', (req, res) => {
+  console.log(req.body);
+  const { id } = req.params;
+  res.send(`update user with id ${id}`);
+})
+
+server.listen(port, () => {
+  console.log('listening on port', port);
+})
