dbbf29d0a3a6412c8b6fde97a7013bf8ad56acb6
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-08 14:57:49 +0000

    feature: Complete form to add new user with bio

--- web-api-fe/src/components/Users/UserForm.js
+++ web-api-fe/src/components/Users/UserForm.js
@@ -1,10 +1,30 @@
 import React from 'react'
 
-export default function UserForm() {
+export default function UserForm({ addUser }) {
+  let nameText = React.createRef();
+  let bioText = React.createRef();
+
+  const onAddUser = (event, nameText) => {
+    event.preventDefault();
+    const user = {
+      name: nameText.current.value,
+      bio: bioText.current.value
+    }
+    addUser(user);
+  }
+  
   return (
-    <form>
-      <input type="text" placeholder="name" />
-      <input type="text" placeholder="bio" />
+    <form onSubmit={event => onAddUser(event, nameText)}>
+      <input
+        type="text"
+        placeholder="name"
+        ref={nameText}
+      />
+      <input
+        type="text"
+        placeholder="bio"
+        ref={bioText}
+      />
       <input type="submit" />
     </form>
   )
--- web-api-fe/src/components/Users/Users.js
+++ web-api-fe/src/components/Users/Users.js
@@ -3,13 +3,15 @@ import React from 'react';
 import User from './User'
 import UserForm from './UserForm';
 
-export default function Users({ users }) {
+export default function Users({ users, addUser, error, isLoading }) {
   return (
     <div>
+      { error && <p>{error}</p>}
+      { isLoading && <p>Loading</p>}
       {
         users.map(user => <User key={user.id} name={user.name} bio={user.bio} />)
       }
-      <UserForm />
+      <UserForm addUser={addUser} />
     </div>
   )
 }
