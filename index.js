7ed380659ccfe9cc0462aac797ce14f5e2eab211
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-08 12:11:42 +0000

    feature: Complete method to get a user with a specified id

--- index.js
+++ index.js
@@ -30,7 +30,15 @@ server.get('/api/users', (req, res) => {
 
 server.get('/api/users/:id', (req, res) => {
   const { id } = req.params;
-  res.send(`get user with ${id}`);
+  userDB.findById(id)
+    .then(user => {
+      !user
+      ? res.status(404).json({ error: "The user with the specified ID does not exist." })
+      : res.status(200).json(user);
+    })
+    .error(() => {
+      res.status(500).json({ error: "The user information could not be retrieved." });
+    })
 })
 
 server.delete('/api/users/:id', (req, res) => {
